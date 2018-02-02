11c10bedafe1d83f427f7e48965158ffa65d92ba
asha <ashaagm@gmail.com>
2018-02-02 11:43:27 -0500

    more packages n their workings

--- liri.js
+++ liri.js
@@ -2,17 +2,45 @@ var env = require("dotenv").config();
 var keys = require("./keys");
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
+var request = require("request");
+var fs = require("fs");
 
+let secondArg = process.argv[2];
+let thirdArg = "";
 
-switch(process.argv[2]) {
-    case 'my-tweets':
-        getTweets();
-        break;
-    case 'spotify-this-song':
-        getSpotify(process.argv[3]);
-        break;
-    default:
-        // code block
+for (let i = 3; i < process.argv.length; i++) {
+
+  if (i > 2 && i < process.argv.length) {
+
+    thirdArg = thirdArg + "+" + process.argv[i];
+
+  } else {
+
+    thirdArg += process.argv[i];
+
+  }
+}
+
+processInputCommand(secondArg, thirdArg);
+
+function processInputCommand(secondArg, thirdArg){
+	switch(secondArg) {
+	    case 'my-tweets':
+	        getTweets();
+	        break;
+	    case 'spotify-this-song':
+	        getSpotify(thirdArg);
+	        break;
+	    case 'movie-this':
+	        getOmdb(thirdArg);
+	        break;  
+	     case 'do-what-it-says':
+	     	getFileData(); 
+	     	break;  
+	    default:
+	        console.log("No input command!");
+	        break;
+	}
 }
 
 function getSpotify(song){
@@ -63,5 +91,46 @@ function getTweets(){
 		}
 	});
 
+}
+
+function getOmdb(movieName){
+
+	movieName = movieName ? movieName : "Mr. Nobody"
+
+	const queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+	console.log(queryUrl);
+
+	request(queryUrl, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			let parsedBody = JSON.parse(body);
+			console.log("-----------------------------------");
+			console.log("Title : " + parsedBody.Title);
+			console.log("Release Year: " + JSON.parse(body).Year);
+			console.log("IMDB Rating : ", parsedBody.imdbRating);
+			console.log("Rotten Tomatoes Rating : ", parsedBody.Ratings[1].Value);
+			console.log("Country of Origin : ", parsedBody.Country);
+			console.log("Language : ", parsedBody.Language);
+			console.log("Plot : ", parsedBody.Plot);
+			console.log("Actors : ", parsedBody.Actors);
+			console.log("-----------------------------------");
+						
+		}
+	});
+
+}
+
+
+function getFileData(){
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		if (error) {
+			return console.log(error);
+		}
+
+		console.log(data);
+		const dataArr = data.split(",");
+
+		processInputCommand(dataArr[0], dataArr[1]);
+
+	});
 }
 
